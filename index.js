0d7b5509df33cb201b4fe589b2edfeb8adc11c74
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-10-02 13:44:13 +0300

    update env port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import "dotenv/config";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
 
 app.use(express.json());
 
@@ -12,6 +12,11 @@ app.get('/', (req, res) => {
     res.send('Server is up and running');
 });
 
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
