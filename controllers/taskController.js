a0d51e3949c8584ea1871058165156046af447df
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-10-02 16:06:15 +0300

    add task controllers create,get,delete...

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,1 +1,36 @@
 // Ali + Abdelrahman + Ahmed
+
+
+// import Notice from "../models/notification.js";
+// import Task from "../models/task.js";
+// import User from "../models/user.js";
+
+export const createTask = async (req, res) => {
+};
+
+export const duplicateTask = async (req, res) => {
+};
+
+export const postTaskActivity = async (req, res) => {
+};
+
+export const dashboardStatistics = async (req, res) => {
+};
+
+export const getTasks = async (req, res) => {
+};
+
+export const getTask = async (req, res) => {
+};
+
+export const createSubTask = async (req, res) => {
+};
+
+export const updateTask = async (req, res) => {
+};
+
+export const trashTask = async (req, res) => {
+};
+
+export const deleteRestoreTask = async (req, res) => {
+};
