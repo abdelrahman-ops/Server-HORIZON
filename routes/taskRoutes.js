96f07c2342b7e1642cb18defcbe1d4c920430d8a
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-10-02 16:02:36 +0300

    add basic tasks routes for api/task/

---
+++ routes/taskRoutes.js
@@ -0,0 +1,41 @@
+import express from "express" ;
+import {isAdminRoute, protectRoute } from "../middleware/authMiddlewares.js"
+import {
+    createSubTask,
+    createTask,
+    dashboardStatistics,
+    deleteRestoreTask,
+    duplicateTask,
+    getTask,
+    getTasks,
+    postTaskActivity,
+    trashTask,
+    updateTask,
+} from "../controllers/taskController.js"
+
+
+const router = express.Router();
+
+router.post("/create", protectRoute , isAdminRoute , createTask);
+router.post("/duplicate/:id", protectRoute , isAdminRoute , duplicateTask);
+router.post("/activity/:id", protectRoute , postTaskActivity);
+
+router.get("/" ,protectRoute , getTasks)
+router.get("/:id" ,protectRoute , getTask)
+router.get("/dashboard" , protectRoute , dashboardStatistics);
+
+
+
+router.put("/create-subtask/:id", protectRoute , isAdminRoute , createSubTask)
+router.put("/update/:id", protectRoute , updateTask);
+router.put("/:id", protectRoute , isAdminRoute , trashTask);
+
+
+router.delete(
+    "/delete-restore/:id",
+    protectRoute,
+    isAdminRoute,
+    deleteRestoreTask
+);
+
+export default router;
