46032c6bc441376e2ec5a9028cf6d1d7c7bf2992
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-10-02 16:05:25 +0300

    add user controllers register,login,delete...

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,36 @@
 // Ali + Abdelrahman + Ahmed
+
+import { response } from "express";
+// import User from "../models/user.js";
+// import { createJWT } from "../utils/index.js";
+// import Notice from "../models/notification.js";
+
+export const registerUser = async (req, res) => {
+};
+
+export const loginUser = async (req, res) => {
+};
+
+export const logoutUser = async (req, res) => {
+};
+
+export const getTeamList = async (req, res) => {
+};
+
+export const getNotificationsList = async (req, res) => {
+};
+
+export const updateUserProfile = async (req, res) => {
+};
+
+export const markNotificationRead = async (req, res) => {
+};
+
+export const changeUserPassword = async (req, res) => {
+};
+
+export const activateUserProfile = async (req, res) => {
+};
+
+export const deleteUserProfile = async (req, res) => {
+};
