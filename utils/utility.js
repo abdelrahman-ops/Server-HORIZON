1493dedfd024f488e8b937084156058c77d7014a
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-10-16 22:31:21 +0300

    update token handling

--- utils/utility.js
+++ utils/utility.js
@@ -8,7 +8,7 @@ export const createJWT = (res, userId) => {
     res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "Lax",
+        sameSite: "None",
         maxAge: 1 * 24 * 60 * 60 * 1000, 
     });
 
