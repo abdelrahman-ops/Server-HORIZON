f91cbed553e09f071a2ee2d4bc4581429ca26714
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-10-18 19:15:15 +0300

    add team schema

--- models/task.js
+++ models/task.js
@@ -49,7 +49,7 @@ const taskSchema = new Schema({
     
     assets: [String],
     
-    // team: [{type: Schema.Types.ObjectId , ref: "User"}],
+    team: [{type: Schema.Types.ObjectId , ref: "User"}],
 
     isTrashed: {type: Boolean , default: false},
 
